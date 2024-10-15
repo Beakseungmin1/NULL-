6ac1948257e6e1dd9645f63ef57de8112892a333
Joyoounjin <dbswls060@gmail.com>
2024-10-15 20:09:37 +0900

    add palyer inputsystem
    - 좌우 이동/ 점프 구현
    - 이동 컨트롤러 스크립트 생성

---
+++ Assets/1. Scripts/Player/TopDownController.cs
@@ -0,0 +1,29 @@
+using System;
+
+using UnityEngine;
+
+
+public class TopDownController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+    public event Action OnJumpEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+    public void CallJumpEvent()
+    {
+        Debug.Log("Jump call");
+        OnJumpEvent?.Invoke();
+    }
+
+}
+
