1ea2ffe596967408a4b3d1f258f5e9d879d29a64
Joyoounjin <dbswls060@gmail.com>
2024-10-16 15:59:34 +0900

    add Player HP and animation
    - 플레이어 스크립트 이름 변경
    - 플레이어 체력 변수 추가
    - 달리기, 더블점프, 추락 애니메이션 추가 및 애니메이터 작업

---
+++ Assets/1. Scripts/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public partial class Player : TopDownController
+{
+    private Camera _camera;
+    private int HP = 3;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    // ----- 체력 ----- 
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+        if (HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // 개임매니저와 연결
+        // 오브젝트 삭제 말고 다른 이벤트 o
+        Destroy(this.gameObject);
+    }
+
+    // ----- 이동 ----- 
+    public void OnMove(InputValue value) // 좌/우 
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void OnLook(InputValue value) // 
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+
+        if (newAim.magnitude >= .9f)
+        {
+            CallLookEvent(newAim);
+        }
+    }
+    public void OnJump(InputValue value) // 점프 
+    {
+        CallJumpEvent();
+    }
+
+    // 더블 점프
+    // 달리기 
+    // 낙하 
+}
+
+
+
