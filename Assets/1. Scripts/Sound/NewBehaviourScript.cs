921eda0610bdb600158f4c1518f7c49f00e12893
이지윤 <wldbs272@naver.com>
2024-10-20 13:37:28 +0900

    Update: Sound List
    -SoundManager스크립트에서 Bgm과 Sfx목록 편집
    -사운드 클립 연결은 아직

---
+++ Assets/1. Scripts/Sound/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // 테스트용
+    public Button TestButton;
+    public void TestButtonClick()
+    {
+        SceneManager.LoadScene("StageScene1"); // 씬 로드
+        SoundManager.instance.PlayBGM(Bgm.BossBgm);
+    }
+}
