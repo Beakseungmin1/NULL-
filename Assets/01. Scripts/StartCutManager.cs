bc5bd563ed949ef3cbc4a6f52806f788c6447bb8
Beakseungmin1 <dhkdwk1456@naver.com>
2024-10-21 14:46:10 +0900

    fix : StartCutScene
    컷신에서 안넘어가던 문제수정

--- Assets/01. Scripts/StartCutManager.cs
+++ Assets/01. Scripts/StartCutManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartEndManager : MonoBehaviour
+public class StartCutManager : MonoBehaviour
 {
     public void LoadCutscene()
     {
