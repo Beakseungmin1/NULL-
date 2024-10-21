26d8ec13d58e937b33b973181593d3ce2acbba91
Joyoounjin <dbswls060@gmail.com>
2024-10-21 10:47:01 +0900

    Reapply "Merge branch 'Dev02' into yj_dev2"
    This reverts commit 8889e2781f2b10ac97893433ed0d0a87b90c6147.

---
+++ Assets/01. Scripts/Enemy/Projectile/FireProjectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    [SerializeField]private float speed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Floor"))
+            this.gameObject.SetActive(false);
+    }
+
+}
