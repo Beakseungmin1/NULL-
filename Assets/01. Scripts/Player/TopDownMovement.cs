14ab54282b62d3c018fd1cfacf57411945d8c9b0
Joyoounjin <dbswls060@gmail.com>
2024-10-21 16:05:36 +0900

    Add Asset
    - 붉은색 구름

--- Assets/01. Scripts/Player/TopDownMovement.cs
+++ Assets/01. Scripts/Player/TopDownMovement.cs
@@ -111,16 +111,8 @@ public class TopDownMovement : MonoBehaviour
     // ------- 피격 -------
     public void Damage(Vector2 targetPos)
     {
-        animator.SetBool("isHit", true);
-        //animator.SetBool("isJumping", false);
-        //animator.SetBool("isDouble", false);
-        //animator.SetBool("isRunning", false);
+        animator.SetTrigger("Hit");
         int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
-    }
-
-    public void StopDamage() // 해제 
-    {
-        animator.SetBool("isHit", false);
     }
 
     // ------ 블록 밟기 ---------
